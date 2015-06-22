c181422bc80c2e04a79b2aa962025cc88afb1086
Mike Acton <macton@gmail.com>
2015-06-22 13:50:15 -0700

    update pkg

---
+++ s.prop.js
@@ -0,0 +1,8 @@
+var S = require('S');
+
+var prop = {
+  get  : function( key, obj ) { return obj?obj[key]:null; },
+  set  : function( key, value, obj ) { obj[key] = value; return obj; },
+};
+
+exports = module.exports = prop;
