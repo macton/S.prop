e4d517a400ef77f073a9d9458e62e7717b7b8133
Mike Acton <macton@gmail.com>
2015-06-23 13:34:33 -0700

    protect prop.set on null object

--- s.prop.js
+++ s.prop.js
@@ -2,7 +2,7 @@ var S = require('s');
 
 var prop = {
   get  : S.curry( function( key, obj ) { return obj?obj[key]:null; } ),
-  set  : S.curry( function( key, value, obj ) { obj[key] = value; return obj; } ),
+  set  : S.curry( function( key, value, obj ) { obj = obj?obj:{}; obj[key] = value; return obj; } ),
 };
 
 exports = module.exports = prop;
--- test/test.js
+++ test/test.js
@@ -4,4 +4,5 @@ S.prop = require( '../s.prop' );
 
 console.log( S.prop.get( 'foo', { foo: 'Foo' } ) );
 console.log( S.prop.set( 'foo', 'Bar', { foo: 'Foo' } ) );
+console.log( S.prop.set( 'foo', 'Bar', null ) );
 
